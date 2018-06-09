222f309175d2b0c535d9814eb7584bf77c8cd5c0
Wonwoo Choi <chwo9843@gmail.com>
2018-06-10 01:43:02 +0900

    :scream: fix indents

--- src/models.rs
+++ src/models.rs
@@ -3,25 +3,25 @@
 use super::schema::{items_ls, category_map_ls};
 
 macro_rules! make_variant_slice {
-	(
-		$(#[ $attr:meta ])*
-		pub enum $enum_name:ident [$slice_name:ident, $name_slice_name:ident] {
-			$($variant:ident,)*
-		}
-	) => {
-		$(#[ $attr ])*
+    (
+        $(#[ $attr:meta ])*
+        pub enum $enum_name:ident [$slice_name:ident, $name_slice_name:ident] {
+            $($variant:ident,)*
+        }
+    ) => {
+        $(#[ $attr ])*
         pub enum $enum_name {
             $($variant,)*
         }
 
-		pub const $slice_name: &'static [$enum_name] = &[
-			$($enum_name::$variant,)*
-		];
+        pub const $slice_name: &'static [$enum_name] = &[
+            $($enum_name::$variant,)*
+        ];
 
         pub const $name_slice_name: &'static [&'static str] = &[
             $(stringify!($variant),)*
         ];
-	};
+    };
 }
 
 make_variant_slice! {
