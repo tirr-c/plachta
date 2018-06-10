b3591be9f3b5c34980702c70622b74bcdac79f65
Wonwoo Choi <chwo9843@gmail.com>
2018-06-10 16:37:46 +0900

    Add necessary indexes

---
+++ migrations/2018-06-10-073324_ls_index/down.sql
@@ -0,0 +1,3 @@
+DROP INDEX category_map_ls_category_index;
+DROP INDEX category_map_ls_item_id_index;
+DROP INDEX items_ls_ty_index;
---
+++ migrations/2018-06-10-073324_ls_index/up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX items_ls_ty_index ON items_ls (ty);
+CREATE INDEX category_map_ls_item_id_index ON category_map_ls (item_id);
+CREATE INDEX category_map_ls_category_index ON category_map_ls (category);
